2b4dc690dcf0f035b92428f75bedbaf467c3f01f
Waipop B <>
2025-05-01 18:14:49 +0700

    no message

--- src/bank/kbank/kbank.service.ts
+++ src/bank/kbank/kbank.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { FirebaseService } from 'src/firebase/firebase.service';
-import { KB_CALLBACK } from 'src/constants/collection-firebase';
+import { KB_AUTH, KB_CALLBACK } from 'src/constants/collection-firebase';
 import axios from 'axios';
 import * as fs from 'fs';
 import * as https from 'https';
@@ -27,15 +27,46 @@ export class KbankService {
 
     async generateKbank(data: { amount: number }): Promise<void> {
         try {
-            const token = await this.getAccessToken();
-            const authHeader = `Bearer ${token.access_token}`;
-            const url = `${this.configService.get('OAUTH_KL')}/v1/qrpayment/request`;
-            const keyPath = this.configService.get('KEY_PATH');
-            const certPath = this.configService.get('CERT_PATH');
-            const key = fs.readFileSync(keyPath);
-            const cert = fs.readFileSync(certPath);
+            const firestore = this.firebaseService.getFirestore();
+            const docRef = firestore.collection(KB_AUTH);
+            const doc = await docRef.doc('auth').get();
+            let access_token: string = '';
+            if (doc.exists) {
+                const dataToken = doc.data();
 
-            const httpsAgent = new https.Agent({ key, cert });
+                if (dataToken?.expired) {
+                    const now = moment();
+                    const expired = moment(dataToken.expired);
+
+                    // เช็คว่าเกิน 29 นาทีหรือยัง (ถ้า now > expired - 29 นาที)
+                    if (now.isBefore(expired.clone().subtract(29, 'minutes'))) {
+                        access_token = dataToken.access_token; // ยังไม่เกิน 29 นาที
+                    }else{
+                        // ถ้าเกิน 29 นาที
+                        const token = await this.getAccessToken();
+                        access_token = token.access_token;
+                        const expired = moment().add(token.expires_in, 'seconds');
+                        await docRef.doc('auth').set({
+                            access_token: token.access_token,
+                            expired: expired.toISOString(),
+                        });
+                    }
+                }
+            }else{
+                // ถ้าไม่มีข้อมูลใน Firestore
+                const token = await this.getAccessToken();
+                access_token = token.access_token;
+                const expired = moment().add(token.expires_in, 'seconds');
+                await docRef.doc('auth').set({
+                    access_token: token.access_token,
+                    expired: expired.toISOString(),
+                });
+            }
+
+
+            const authHeader = `Bearer ${access_token}`;
+            const url = `${this.configService.get('OAUTH_KL')}/v1/qrpayment/request`;
+            const { agent } = await this.createHttpsAgent();
             const now = moment();
             const param = {
                 merchantId: this.configService.get('MERCHANT_ID_KL'),
@@ -57,7 +88,7 @@ export class KbankService {
                     'Content-Type': 'application/json',
                     'env-id': 'QR002',
                 },
-                httpsAgent,
+                httpsAgent: agent,
             });
             return response.data;
         } catch (error) {
@@ -85,20 +116,17 @@ export class KbankService {
             const encoded = Buffer.from(`${consumerId}:${consumerSecret}`).toString('base64');
             const authHeader = `Basic ${encoded}`;
 
-            const keyPath = this.configService.get('KEY_PATH');
-            const certPath = this.configService.get('CERT_PATH');
-            const key = fs.readFileSync(keyPath);
-            const cert = fs.readFileSync(certPath);
+            const { agent } = await this.createHttpsAgent();
 
-            const httpsAgent = new https.Agent({ key, cert });
+            // const httpsAgent = new https.Agent({ key, cert });
 
-           
+
             const response = await axios.post(url, payload, {
                 headers: {
                     Authorization: authHeader,
                     'Content-Type': 'application/x-www-form-urlencoded',
                 },
-                httpsAgent,
+                httpsAgent: agent,
             });
             return response.data;
         } catch (error) {
--- src/constants/collection-firebase.ts
+++ src/constants/collection-firebase.ts
@@ -1,2 +1,2 @@
 export const KB_CALLBACK = "KB_Callback";
-  
+export const KB_AUTH = "KB_AUTH";
